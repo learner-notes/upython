5f4cbaaba938d7694f2275c20856219298aadf62
learner-notes <76621083+learner-notes@users.noreply.github.com>
2021-01-02 21:19:37 +0530

    Add files via upload

---
+++ o.js
@@ -0,0 +1,16 @@
+function display_nav() {
+  if (document.getElementById('dropdown').style.display == "block") {
+    document.getElementById('dropdown').style.display = "none";
+  } else {
+    document.getElementById('dropdown').style.display = "block";
+    document.getElementById('dropdown').innerHTML = "<a href='o.css'>INTRODUCTION</a><a>link 2</a><a>link 3</a><a>link 4</a>";
+  }
+}
+function main_block(x) {
+  var y = document.getElementsByClassName('topics-displayer');
+  for (var i = 0; i < y.length; i++) {
+    y[i].style.backgroundColor = "#d3d3d3";
+  }
+  x.style.backgroundColor = "#ff4500";
+  document.getElementById('content-displayer').innerHTML = document.getElementById(x.innerHTML).innerHTML;
+}
